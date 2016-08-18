345d343f30d4e818c1881181273a2015876a8d32
Lucas Shen <lucas.shen@sqor.com>
2016-08-18 14:56:00 -0700

    fine tune data retrieval method

--- helper/general.py
+++ helper/general.py
@@ -1,13 +1,20 @@
 import numpy as np
+import pandas as pd
 
 
 def get_X(df):
-    return df.iloc[:, :-1].as_matrix()
+    """
+    use concat to add intersect feature to avoid side effect
+    not efficient for big dataset though
+    """
+    ones = pd.DataFrame({'ones': np.ones(len(df))})
+    data = pd.concat([ones, df], axis=1)  # column concat
+    return data.iloc[:, :-1].as_matrix()  # this return ndarray, not matrix
 
 
 def get_y(df):
     '''assume the last column is the target'''
-    return np.array(df.iloc[:, -1])
+    return np.array(df.iloc[:, -1]).reshape(len(df), 1)  # explicit shape for tensorflow
 
 
 def normalize_feature(df):
