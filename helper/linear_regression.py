d58692f5f4aaad3fcf8893fa9c91b63796f288b6
icrtiou <icrtiou@gmail.com>
2016-08-18 21:20:23 -0700

    rearange tf code into modules

--- helper/linear_regression.py
+++ helper/linear_regression.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+import numpy as np
+
+
 def compute_cost(X, y, theta):
     """
     X: R(m*n), m records, n features
@@ -9,7 +13,7 @@ def compute_cost(X, y, theta):
 
     cost = square_sum / (2 * (len(X)))
 
-    return cost
+    return cost[0, 0]  # 1*1 ndarray, get the value out
 
 
 def batch_update_theta(X, y, theta, alpha):
@@ -36,3 +40,40 @@ def batch_gradient_decent(X, y, theta, alpha, epoch):
         cost.append(compute_cost(X, y, _theta))
 
     return _theta, cost
+
+
+def linear_regression(X_data, y_data, alpha, epoch, optimizer=tf.train.GradientDescentOptimizer):
+    """tensorflow implementation"""
+    # placeholder for graph input
+    X = tf.placeholder(tf.float32, shape=X_data.shape)
+    y = tf.placeholder(tf.float32, shape=y_data.shape)
+
+    # construct the graph
+    with tf.variable_scope('linear-regression'):
+        W = tf.get_variable("weights",
+                            (X_data.shape[1], 1),
+                            initializer=tf.constant_initializer())  # n*1
+
+        y_pred = tf.matmul(X, W)  # m*n @ n*1 -> m*1
+
+        loss = 1 / (2 * len(X_data)) * tf.matmul((y_pred - y), (y_pred - y), transpose_a=True)  # (m*1).T @ m*1 = 1*1
+
+    opt = optimizer(learning_rate=alpha)
+    opt_operation = opt.minimize(loss)
+
+    # run the session
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        loss_data = []
+
+        for i in range(epoch):
+            _, loss_val, W_val = sess.run([opt_operation, loss, W], feed_dict={X: X_data, y: y_data})
+            loss_data.append(loss_val[0, 0])  # because every loss_val is 1*1 ndarray
+
+            if len(loss_data) > 1 and np.abs(loss_data[-1] - loss_data[-2]) < 10 ** -9:  # early break when it's converged
+                print('Converged at epoch {}'.format(i))
+                break
+
+    # clear the graph
+    tf.reset_default_graph()
+    return {'loss': loss_data, 'parameters': W_val}  # just want to return in row vector format
