3dbdcd69363572b34846b82be6f16e6365093d1c
Lucas Shen <lucas.shen@sqor.com>
2016-08-18 17:11:17 -0700

    gradient took me a while, got caught in vector computation. keep working..

---
+++ helper/logistic_regression.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def cost(X, y, theta):
+    ''' cost fn is -l(theta) for you to minimize'''
+    return np.mean(-y * np.log(sigmoid(X @ theta)) - (1 - y) * np.log(1 - sigmoid(X @ theta)))
+
+
+def gradient(X, y, theta):
+    '''just 1 batch gradient'''
+    return (1 / len(X)) * X.T @ (sigmoid(X @ theta) - y)
