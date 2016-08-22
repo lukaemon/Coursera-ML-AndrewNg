e197d8f92854b68637901842c6acbde6ecec49fa
icrtiou <icrtiou@gmail.com>
2016-08-21 17:52:35 -0700

    boundary is done, call it a day

--- helper/logistic_regression.py
+++ helper/logistic_regression.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import scipy.optimize as opt
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from helper import general as general
 
 
 def sigmoid(z):
@@ -61,17 +65,18 @@ def regularized_gradient(theta, X, y, l=1):
     return gradient(theta, X, y) + regularized_term
 
 
-def logistic_regression(x1, x2, y, power, l):
+def logistic_regression(power, l):
     """
-    x1, x2: ndarray
-        feature vectors
-    y: ndarray
-        target vector
     power: int
         raise x1, x2 to polynomial power
     l: int
         lambda constant for regularization term
     """
+    df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
+    x1 = np.array(df.test1)
+    x2 = np.array(df.test2)
+    y = general.get_y(df)
+
     X = feature_mapping(x1, x2, power, as_ndarray=True)
     theta = np.zeros(X.shape[1])
 
@@ -86,3 +91,36 @@ def logistic_regression(x1, x2, y, power, l):
 
     return {'accuracy': accuracy,
             'theta': final_theta}
+
+
+def find_decision_boundary(density, power, theta, threshhold):
+    t1 = np.linspace(-1, 1.5, density)
+    t2 = np.linspace(-1, 1.5, density)
+
+    cordinates = [(x, y) for x in t1 for y in t2]
+    x_cord, y_cord = zip(*cordinates)
+    mapped_cord = feature_mapping(x_cord, y_cord, power)  # this is a dataframe
+
+    inner_product = mapped_cord.as_matrix() @ theta
+
+    decision = mapped_cord[np.abs(inner_product) < threshhold]
+
+    return decision.f10, decision.f01
+
+
+def draw_boundary(power, l):
+    """
+    power: polynomial power for mapped feature
+    l: lambda constant
+    """
+    density = 1000
+    threshhold = 2 * 10**-3
+
+    final_theta = logistic_regression(power, l)['theta']
+    a, b = find_decision_boundary(density, power, final_theta, threshhold)
+
+    df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
+    sns.lmplot('test1', 'test2', hue='accepted', data=df, size=8, fit_reg=False, scatter_kws={"s": 100})
+
+    plt.scatter(a, b, c='R', s=10)
+    plt.title('Decision boundary')
