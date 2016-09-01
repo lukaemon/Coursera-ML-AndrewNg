f60739eadd2b1ab3077fbba7e95b9d8c7d42c70a
Lucas Shen <lucas.shen@sqor.com>
2016-09-01 12:05:42 -0700

    one more test for expand_y fn

--- helper/test/nn_test.py
+++ helper/test/nn_test.py
@@ -2,10 +2,18 @@ import helper.nn as nn
 import numpy as np
 
 
+def test_expand_y():
+    _, y = nn.load_data('./helper/test/test_data/ex4data1.mat')
+
+    expand = nn.expand_y(y)
+
+    assert np.all(np.argmax(expand, axis=1) + 1 == y)  # map np index to raw data
+
+
 def test_serial_deserial():
     t1, t2 = nn.load_weight('./helper/test/test_data/ex4weights.mat')
 
     o1, o2 = nn.deserialize(nn.serialize(t1, t2))
 
-    assert np.mean(o1 == t1) == 1.0
-    assert np.mean(o2 == t2) == 1.0
+    assert np.all(o1 == t1)
+    assert np.all(o2 == t2)
