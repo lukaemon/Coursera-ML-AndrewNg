3b6c3ea5ad6ae51b90db1f05e628009d198692ad
Lucas Shen <lucas.shen@sqor.com>
2016-09-01 10:34:36 -0700

    god, this gradeint stuff is mind bending..

--- helper/nn.py
+++ helper/nn.py
@@ -116,3 +116,35 @@ def sigmoid_gradient(z):
     pairwise op is key for this to work on vector and matrix
     """
     return np.multiply(lr.sigmoid(z), 1 - lr.sigmoid(z))
+
+
+def gradient(t1, t2, X, y):
+    # initialize
+    m = X.shape[0]
+
+    delta1 = np.zeros(t1.shape)  # (25, 401)
+    delta2 = np.zeros(t2.shape)  # (10, 26)
+
+    a1, z2, a2, z3, h = feed_forward(t1, t2, X)
+
+    for i in range(m):
+        a1i = a1[i, :]  # (1, 401)
+        z2i = z2[i, :]  # (1, 25)
+        a2i = a2[i, :]  # (1, 26)
+
+        hi = h[i, :]    # (1, 10)
+        yi = y[i, :]    # (1, 10)
+
+        d3i = hi - yi  # (1, 10)
+
+        z2i = np.insert(z2i, 0, np.ones(1))  # make it (1, 26) to compute d2i
+        d2i = np.multiply(t2.T @ d3i, sigmoid_gradient(z2i))  # (1, 26)
+
+        # careful with np vector transpose
+        delta2 += np.matrix(d3i).T @ np.matrix(a2i)  # (1, 10).T @ (1, 26) -> (10, 26)
+        delta1 += np.matrix(d2i[1:]).T @ np.matrix(a1i)  # (1, 25).T @ (1, 401) -> (25, 401)
+
+    delta1 = delta1 / m
+    delta2 = delta2 / m
+
+    return delta1, delta2
