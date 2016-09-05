8a7fb40cde0afb2530a8049bce2227928379a5c2
icrtiou <icrtiou@gmail.com>
2016-09-05 12:08:03 -0700

    spend too much time on vis... just get it work and move on

--- helper/general.py
+++ helper/general.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import altair as alt
 
 
 def get_X(df):
@@ -20,19 +19,3 @@ def get_y(df):
 
 def normalize_feature(df):
     return df.apply(lambda s: (s - s.mean()) / s.std())
-
-
-def scatter_plot(x, y, x_name, y_name):
-    df = pd.DataFrame({x_name: x, y_name: y})
-    c = alt.Chart(df).mark_circle().encode(
-        x=x_name,
-        y=y_name)
-    return c
-
-
-def line_plot(x, y, x_name, y_name):
-    df = pd.DataFrame({x_name: x, y_name: y})
-    c = alt.Chart(df).mark_line().encode(
-        x=x_name,
-        y=y_name)
-    return c
