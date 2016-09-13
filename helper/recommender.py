d8db0d9dc9966835cddb446d91c3880688763776
Lucas Shen <lucas.shen@sqor.com>
2016-09-13 16:56:53 -0700

    redo cost fn

--- helper/recommender.py
+++ helper/recommender.py
@@ -27,8 +27,8 @@ def cost(param, Y, R, n_features):
     # theta (user, feature), (943, 10): user preference
     # X (movie, feature), (1682, 10): movie features
     n_movie, n_user = Y.shape
-
     X, theta = deserialize(param, n_movie, n_user, n_features)
-    inner = (X @ theta.T)[R == 1] - Y[R == 1]
 
-    return np.dot(inner, inner) / 2
+    inner = np.multiply(X @ theta.T - Y, R)
+
+    return np.power(inner, 2).sum() / 2
